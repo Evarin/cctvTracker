ae83b953af102c12f1d8fc3c9cf03033fad7d8cf
Andreea Beica <beica@clipper.ens.fr>
2014-02-21 23:28:50 +0100

    added location extraction from Google maps KML files

--- location.py
+++ location.py
@@ -1,4 +1,4 @@
-# Structure de données et extraction des coordonnées GPS / KML
+# Structure de donnees et extraction des coordonnees GPS / KML
 
 class Location:
     
@@ -8,3 +8,27 @@ class Location:
 
     def __repr__(self):
         return str(self.north) + "N, " + str(self.east) + "E"
+
+
+
+from xml.dom.minidom import parseString
+
+def findMyLocation(location):
+
+    myLocation= []
+
+    #read KML file as a string
+    file = open(location)
+    data = file.read()
+    file.close()
+
+    #parse string into DOM
+    dom = parseString(data)
+
+    #retrieve coordinates, whatever the namespace
+    for d in dom.getElementsByTagNameNS('*','coord'):
+        #in kml doc, first is longitude and second is latitude
+        coords=d.firstChild.data.split(' ')
+        x=Location(float(coords[1]),float(coords[0]))
+        myLocation.append(x)
+    return myLocation
